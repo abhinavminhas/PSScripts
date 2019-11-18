1aac262233073c040a8c8cd73f4db0b4d579eb58
Abhinav Minhas <abhinav.minhas@unimelb.edu.au>
2019-11-18 16:39:09 +1100

    Switch-ProcessorArchitecture64 Cmdlet Update

--- Cmdlets/Switch-ProcessorArchitecture64-Cmdlet.ps1
+++ Cmdlets/Switch-ProcessorArchitecture64-Cmdlet.ps1
@@ -4,27 +4,43 @@
 This function starts the Powershell IDE in 64-bit mode.
 
 .Description
-This function checks if Powershell is running the 32-bit version on a 64-bit machine and forces powershell to run in 64-bit mode.
+This function checks if Powershell is running the 32-bit version on a 64-bit machine and forces Powershell to run in 64-bit mode.
 
 .Example
 Switch-ProcessorArchitecture64
-This command checks the 32-bit version powershell on a 64-bit machine and switches to 64-bit mode.
+This command checks the 32-bit version Powershell on a 64-bit machine and switches Powershell to 64-bit mode.
 
 #>
 
-Function Switch-ProcessorArchitecture64()
+Function Switch-ProcessorArchitecture64
 {
-    if ($env:PROCESSOR_ARCHITEW6432 -eq "AMD64")
+    Begin
     {
-        Write-Log "....Off To 64-bit....."
-        if ($myInvocation.Line)
+        Function Write-Log([string]$logMessage)
         {
-            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile $myInvocation.Line
+            $print = $logMessage.length + 6
+            Write-Output $("-" * ($print))
+            Write-Output "[[ $logMessage ]]"
+            write-output $("-" * ($print))
         }
-        else
+    }
+
+    Process
+    {
+        if ($env:PROCESSOR_ARCHITEW6432 -eq "AMD64")
         {
-            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -file "$($myInvocation.InvocationName)" $args
+            Write-Log "....Off To 64-bit....."
+            if ($myInvocation.Line)
+            {
+                &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile $myInvocation.Line
+            }
+            else
+            {
+                &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -file "$($myInvocation.InvocationName)" $args
+            }
+            exit $lastexitcode
         }
-        exit $lastexitcode
     }
 }
+
+#Switch-ProcessorArchitecture64
